66620f220083e1e491a5432bb5c0f4be791a4f59
Daniel Francis <bookemdano@gmail.com>
2022-12-11 10:41:29 -0500

    More 11

--- Advent22/Day11.cs
+++ Advent22/Day11.cs
@@ -1,4 +1,9 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Net;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
 using static System.Formats.Asn1.AsnWriter;
 using static System.Net.Mime.MediaTypeNames;
 
@@ -8,12 +13,12 @@ namespace Advent22
     {
         static public void Run()
         {
-            //Star1();
+            Star1();
             Star2();
         }
         static public void Star1()
         {
-            var input = File.ReadAllLines("Day11.txt");
+            var input = File.ReadAllLines("DayFake11.txt");
             var monkeys = new List<Monkey>();
             for(int i = 0; i < input.Count(); i += 7)
                 monkeys.Add(new Monkey(input[i + 1], input[i + 2], input[i + 3], input[i + 4], input[i + 5]));
@@ -46,8 +51,9 @@ namespace Advent22
             for (int i = 0; i < input.Count(); i += 7)
                 monkeys.Add(new Monkey(input[i + 1], input[i + 2], input[i + 3], input[i + 4], input[i + 5]));
 
-            for (int round = 0; round < 1000; round++)
+            for (int round = 0; round < 20; round++)
             {
+                Helper.Log("Round " + round);
                 for (int i = 0; i < monkeys.Count(); i++)
                 {
                     var monkey = monkeys[i];
@@ -68,37 +74,166 @@ namespace Advent22
             Helper.Log("Star2 Score: " + score);
         }
     }
+    class ItemBi
+    {
+        public ItemBi(long worryLevel)
+        {
+            _bi = new BigInteger(worryLevel);
+        }
+        public override string ToString()
+        {
+            return _bi.ToString();
+        }
+
+        private BigInteger _bi;
+
+        internal bool IsDivisible(int denominator)
+        {
+            return _bi % denominator == 0;
+        }
+        internal void Square()
+        {
+            _bi = _bi * _bi;
+        }
+        internal void Multiply(int prime)
+        {
+            _bi = _bi * prime;
+        }
+        internal void Divide(int denominator)
+        {
+            _bi = _bi / denominator;
+        }
+        internal void Add(int n)
+        {
+            _bi = _bi + n;
+        }
+    }
+
     class Item
     {
-        static int[] _primes = new int[] { 2, 3, 5, 7, 11, 13, 17, 19 };
+        //private BigInteger _bi;
+        static List<int> _primes = new List<int>();
+        static Item()
+        {
+            int next = 2;
+            while(_primes.Count() < 1000)
+            {
+                bool comp = false;
+                foreach (var prime in _primes)
+                {
+                    if (next % prime == 0)
+                    {
+                        comp = true;
+                        break;
+                    }
+                }
+                if (!comp)
+                    _primes.Add(next);
+                next++;
+            }
+        }
+
         public Item(long worryLevel)
         {
-            if (worryLevel == 1)
+            _dict = Factor(worryLevel);
+            //_bi = worryLevel;
+        }
+        static Dictionary<int, int> Factor(BigInteger level)
+        {
+            var rv = new Dictionary<int, int>();
+            if (level == 1)
             {
-                _dict.Add(1, 1);
-                return;
+                rv.Add(1, 1);
+                return rv;
             }
-            for (int i = 2; i < 1000; i++)
+            foreach(var prime in _primes)
             {
-                while (worryLevel % i == 0)
+                bool containsKey = rv.ContainsKey(prime);
+                while (level % prime == 0)
                 {
-                    if (!_dict.ContainsKey(i))
-                        _dict.Add(i, 0);
-                    _dict[i]++;
-                    worryLevel = worryLevel / i;
+                    if (!containsKey)
+                    {
+                        rv.Add(prime, 0);
+                        containsKey = true;
+                    }
+                    rv[prime]++;
+                    level = level / prime;
                 }
-                if (worryLevel == 1)
+                if (level == 1)
                     break;
             }
+            if (level != 1)
+            {
+                // now we have to use the remainer
+                _remainder = (long)level;
+                if (_remainder < 0)
+                    Helper.Log("Bad News");
+            }
+            return rv;
         }
         public override string ToString()
         {
             return string.Join(",", _dict.Select(kvp => $"{kvp.Key}^{kvp.Value}"));
         }
 
-        public long WorryLevel { get; set; }
-
         Dictionary<int, int> _dict = new Dictionary<int, int>();
+        private static long _remainder = 1;
+
+        internal bool IsDivisible(int denominator)
+        {
+            //Debug.Assert((_dict.ContainsKey(denominator)) == (_bi % denominator == 0));
+            return (_dict.ContainsKey(denominator));
+        }
+        internal void Square()
+        {
+            foreach(var k in _dict.Keys)
+                _dict[k] = _dict[k] * 2;
+            //_bi = _bi * _bi;
+            //Debug.Assert(ToBi() == _bi);
+        }
+        internal void Multiply(int prime)
+        {
+            if (!_dict.ContainsKey(prime))
+                _dict.Add(prime, 1);
+            else
+                _dict[prime] = _dict[prime] * 2;
+
+            //_bi = _bi * prime;
+            //Debug.Assert(ToBi() == _bi);
+        }
+        internal void DivideBy3()
+        {
+            if (_dict.ContainsKey(3))
+            {
+                _dict[3]--;
+                if (_dict[3] == 0)
+                    _dict.Remove(3);
+            }
+            else
+            {
+                var bi = ToBi();
+                _dict = Factor(bi / 3);
+            }
+
+            //_bi = _bi / 3;
+            //Debug.Assert(ToBi() == _bi);
+        }
+        BigInteger ToBi()
+        {
+            BigInteger bi = 1;
+            foreach (var kvp in _dict)
+                bi = bi * new BigInteger(Math.Pow(kvp.Key, kvp.Value));
+            return bi;
+        }
+        internal void Add(int n)
+        {
+            var bi = ToBi();
+            bi += n;
+            _dict = Factor(bi);
+
+            //_bi = _bi + n;
+            //Debug.Assert(ToBi() == _bi);
+        }
     }
 
     class Monkey
@@ -122,30 +257,29 @@ namespace Advent22
         }
         internal bool Test(Item item)
         {
-            return (item.WorryLevel % Denominator == 0);
+            return item.IsDivisible(Denominator);
         }
-        internal double OperateOn(Item item)
+        internal void OperateOn(Item item)
         {
             InspectionCount++;
             if (Operation == "* old")
-                item.WorryLevel = item.WorryLevel * item.WorryLevel;
+                item.Square();
             else
             {
                 var parts = Operation.Split(" ");
                 var val = int.Parse(parts[1]);
                 if (parts[0] == "+")
-                    item.WorryLevel = item.WorryLevel + val;
+                    item.Add(val);
                 if (parts[0] == "*")
-                    item.WorryLevel = item.WorryLevel * val;
+                    item.Multiply(val);
             }
-            if (item.WorryLevel < 0)
-                Helper.Log("Bad news!");
-            return item.WorryLevel;
+            //if (item.WorryLevel < 0)
+            //    Helper.Log("Bad news!");
         }
-        internal double Relax(Item item)
+        internal void Relax(Item item)
         {
-            item.WorryLevel = (long)((double)item.WorryLevel / 3.0);
-            return item.WorryLevel;
+            item.DivideBy3();
+            //return item.WorryLevel;
         }
 
         internal void ThrowTo(Monkey monkey, Item item)
