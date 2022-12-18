69550660f29b7f21a672eec5f9317a387bc4ff7d
Daniel Francis <bookemdano@gmail.com>
2022-12-17 19:01:58 -0500

    Day 16

--- Advent22/Day16.cs
+++ Advent22/Day16.cs
@@ -1,16 +1,243 @@
-﻿using System.ComponentModel.Design;
-using System.Diagnostics;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
-using static Advent22.Days.Day14;
-using static System.Net.Mime.MediaTypeNames;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
 
 namespace Advent22
 {
     internal class Day16
     {
+        public class Conn
+        {
+            public Conn(string name)
+            {
+                Name = name;
+            }
+            public string Name { get; }
+            public int Distance { get; set;  }
+            public int Index
+            {
+                get
+                {
+                    return ValveList.GetIndex(Name);
+                }
+            }
+
+        }
+        public class ValveList
+        {
+            static List<string> _names = new List<string>();
+            internal static int GetIndex(string name)
+            {
+                return _names.IndexOf(name);
+            }
+            public ValveList(string[] input)
+            {
+                Valves = new List<Valve>();
+                foreach (var line in input)
+                {
+                    var valve = Valve.Parse(line);
+                    _names.Add(valve.Name);
+                    if (valve != null)
+                        Valves.Add(valve);
+                }
+
+                Graph = new int[Valves.Count, Valves.Count];
+                for (int x = 0; x < Valves.Count; x++)
+                {
+                    var xName = Valves[x].Name;
+                    for (int y = 0; y < Valves.Count; y++)
+                    {
+                        if (x == y)
+                        {
+                            Graph[x, y] = 0;
+                            continue;
+                        }
+
+                        var yName = Valves[y].Name;
+                        if (Valves[x].Connections.Any(c => c.Name == yName))
+                            Graph[x, y] = 1;
+                        else
+                            Graph[x, y] = 9;
+                    }
+                }
+                Distances = FloydWarshall(Graph, Valves.Count);
+                foreach(var valve in Valves)
+                {
+                    foreach(var conn in valve.Connections)
+                        conn.Distance = Distances[valve.Index, conn.Index];
+                }
+                Draw();
+            }
+            
+            // https://www.csharpstar.com/floyd-warshall-algorithm-csharp/
+            public static int[,] FloydWarshall(int[,] graph, int verticesCount)
+            {
+                int[,] distance = new int[verticesCount, verticesCount];
+                for (int i = 0; i < verticesCount; ++i)
+                    for (int j = 0; j < verticesCount; ++j)
+                        distance[i, j] = graph[i, j];
+                for (int k = 0; k < verticesCount; ++k)
+                {
+                    for (int i = 0; i < verticesCount; ++i)
+                    {
+                        for (int j = 0; j < verticesCount; ++j)
+                        {
+                            if (distance[i, k] + distance[k, j] < distance[i, j])
+                                distance[i, j] = distance[i, k] + distance[k, j];
+                        }
+                    }
+                }
+                return distance;
+            }
+            void Draw()
+            {
+                var n = Valves.Count;
+                if (n > 10)
+                    return;
+                for (int x = 0; x < n; x++)
+                {
+                    var line = "";
+                    for (int y = 0; y < n; y++)
+                    {
+                        line += " " + Graph[x, y];
+                    }
+                    Helper.Log(line);
+                }
+                if (Distances != null)
+                {
+                    Helper.Log("Distances");
+                    for (int x = 0; x < n; x++)
+                    {
+                        var line = "";
+                        for (int y = 0; y < n; y++)
+                        {
+                            line += " " + Distances[x, y];
+                        }
+                        Helper.Log(line);
+                    }
+                }
+            }
+            public List<Valve> Valves { get; set; }
+            public int[,] Graph { get; }
+            public int[,] Distances { get; }
+
+            internal List<Valve> GetUseful()
+            {
+                return Valves.Where(v => v.Rate > 0).ToList();
+            }
+
+            internal int GetFlow(int daysRemaining, IEnumerable<Valve> goodValves, Valve from)
+            {
+                int max = 0;
+                foreach (var to in goodValves)
+                {
+                    var days = daysRemaining - Distances[from.Index, to.Index] - 1;
+                    if (days > 0)
+                    {
+                        var add = days * to.Rate + GetFlow(days, goodValves.Where(v => to.Name != v.Name), to);
+                        if (add > max)
+                            max = add;
+                    }
+                }
+                return max;
+            }
+            internal int GetFlowDouble(int daysRemaining, IEnumerable<Valve> goodValves, Valve from)
+            {
+                int max = 0;
+                foreach (var to in goodValves)
+                {
+                    var days = daysRemaining - Distances[from.Index, to.Index] - 1;
+                    if (days > 0)
+                    {
+                        var add = days * to.Rate + GetFlow(days, goodValves.Where(v => to.Name != v.Name), to);
+                        if (add > max)
+                            max = add;
+                    }
+                }
+                return max;
+            }
+        }
+        public class DFSGraph
+        {
+            LinkedList<int>[] _edges;
+            private List<int> _vals;
+            int _total = 0;
+            public DFSGraph(int size, List<int> vals)
+            {
+                _edges= new LinkedList<int>[size];
+                _vals = vals;
+            }
+            public void AddEdge(int from, int to)
+            {
+                if (_edges[from] == null)
+                {
+                    _edges[from] = new LinkedList<int>();
+                    _edges[from].AddFirst(to);
+                }
+                else
+                {
+                    var last = _edges[from].Last;
+                    _edges[from].AddAfter(last, to);
+                }
+            }
+            public void InternalDoIt(int from, bool[] seen)
+            {
+                seen[from] = true;
+                
+                if (_edges[from] != null)
+                {
+                    foreach(var v in _edges[from])
+                    {
+                        if (seen[v] != true)
+                        {
+                            InternalDoIt(v, seen);
+                        }
+                    }
+                }
+            }
+
+            internal void DoIt(int from)
+            {
+                var seen = new bool[_edges.Length + 1];
+                InternalDoIt(from, seen);
+            }
+        }
+        public class Valve
+        {
+            public string Name { get; set; }
+            public int Rate { get; set; }
+            public List<Conn> Connections { get; set; }
+            public bool Opened { get; private set; }
+            public int Index
+            {
+                get
+                {
+                    return ValveList.GetIndex(Name);
+                }
+            }
+            static public Valve Parse(string line)
+            {
+
+                var halves = line.Split(';');
+                var parts = halves[0].Split("=; ".ToCharArray());
+                var rate = int.Parse(parts[5]);
+                var rv = new Valve();
+                rv.Rate = rate;
+                rv.Name = parts[1];
+                parts = halves[1].Substring(23).Trim().Split(", ");
+                rv.Connections = halves[1].Substring(23).Trim().Split(", ").ToList().Select(s => new Conn(s)).ToList();
+                return rv;
+            }
+
+            public override string ToString()
+            {
+                var rv = $"{Name} r:{Rate} {(Opened ? "O" : "X")}";
+                if (Connections?.Any() == true)
+                    rv += $" to:{string.Join(",", Connections)}";
+                return rv;
+            }
+
+
+        }
         static public void Run()
         {
             Day1();
@@ -18,15 +245,21 @@ namespace Advent22
         }
         static void Day1()
         {
-            var input = File.ReadAllLines("DayFake15.txt");
-            var score = 0;
-            Helper.Log("Star1 Score: " + score); 
+            var input = File.ReadAllLines("Day16.txt");
+            var valveList = new ValveList(input);
+            var pressure = valveList.GetFlow(30,
+                                   valveList.GetUseful(),
+                                   valveList.Valves[0]);
+            Helper.Log("Star1 Score: " + pressure); 
         }
         static void Day2()
         {
-            var input = File.ReadAllLines("DayFake15.txt");
-            var score = 0;
-            Helper.Log("Star2 Score: " + score);
+            var input = File.ReadAllLines("Day16.txt");
+            var valveList = new ValveList(input);
+            var pressure2 = valveList.GetFlowDouble(26,
+                                   valveList.GetUseful(),
+                                   valveList.Valves[0]);
+            Helper.Log("Star2 Score: " + pressure2);
         }
     }
 }
---
+++ Advent22/Day16b.cs
@@ -0,0 +1,270 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using static Advent22.Day16;
+using static Advent22.Days.Day14;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Advent22
+{
+    internal class Day16b
+    {
+        public class ValveList
+        {
+            public ValveList(string[] input)
+            {
+                Valves = new List<Valve>();
+                foreach (var line in input)
+                {
+                    var valve = Valve.Parse(line);
+                    if (valve != null)
+                        Valves.Add(valve);
+                }
+
+                // connect
+                foreach (var valve in Valves)
+                    valve.Connect(Valves);
+
+            }
+
+            public List<Valve> Valves { get; set; }
+            public void Prune()
+            {
+                var keepGoing = true;
+                while (keepGoing)
+                {
+                    var removes = new List<Valve>();
+                    keepGoing = false;
+                    foreach (var valve in Valves)
+                    {
+                        if (valve.Prune(Valves))
+                            keepGoing = true;
+                        if (!valve.Connections.Any())
+                        {
+                            removes.Add(valve);
+                            keepGoing = true;
+                        }
+                    }
+                    foreach (var valve in removes)
+                        Valves.Remove(valve);
+                }
+                Valves = Valves.OrderByDescending(v => v.Rate).ToList();
+            }
+
+            internal Valve Move()
+            {
+                return Valves.FirstOrDefault(v => v.Opened == false);
+            }
+
+            internal int Flow()
+            {
+                return Valves.Where(v => v.Opened).Sum(v => v.Rate);
+            }
+        }
+
+        public class Valve
+        {
+            public string Name { get; set; }
+            public int Rate { get; set; }
+            string _connectionString { get; set; }
+            public List<Valve> Connections { get; set; }
+            public bool Opened { get; private set; }
+            public bool Closed
+            {
+                get
+                {
+                    return !Opened;
+                }
+            }
+            public void Open()
+            {
+                Opened = true;
+                Helper.Log("Opened " + this);
+            }
+
+            static public Valve Parse(string line)
+            {
+
+                var halves = line.Split(';');
+                var parts = halves[0].Split("=; ".ToCharArray());
+                var rate = int.Parse(parts[5]);
+                var rv = new Valve();
+                rv.Rate = rate;
+                rv.Name = parts[1];
+                rv._connectionString = halves[1].Substring(23).Trim();
+                return rv;
+            }
+            public void Connect(List<Valve> valves)
+            {
+                var parts = _connectionString.Split(", ");
+                Connections = new List<Valve>();
+                foreach (var part in parts)
+                {
+                    var found = valves.FirstOrDefault(v => v.Name == part);
+                    if (found != null)
+                        Connections.Add(found);
+                }
+            }
+
+            public bool Prune(List<Valve> valves)
+            { 
+                if (!Connections.Any())
+                    return false;
+
+                var removes = new List<Valve>();
+                bool change = false;
+                foreach(var conn in Connections)
+                {
+                    if (!valves.Contains(conn))
+                        removes.Add(conn);
+                }
+
+                foreach (var valve in removes)
+                {
+                    Connections.Remove(valve);
+                    change = true;
+                }
+                return change;
+            }
+            public override string ToString()
+            {
+                var rv = $"{Name} r:{Rate} {(Opened?"O":"X")}";
+                if (Connections?.Any() == true)
+                    rv += $" to:{string.Join(",", Connections.Select(c => c.Name))}";
+                return rv;
+            }
+
+
+            internal Valve Move(int daysRemaining)
+            {
+                var options = new List<ValveOption>();
+                var rate = Rate * (daysRemaining);
+                options.Add(new ValveOption(this, rate));
+                int level = 5;
+                foreach (var conn in Connections)
+                {
+                    var option = new ValveOption(conn, conn.Rate * (daysRemaining - 1));
+                    foreach (var conn2 in conn.Connections)
+                    {
+                        rate = conn2.Rate * (daysRemaining - 2);
+                        if (rate > option.Rate)
+                            option.Rate = rate;
+                    }
+                    options.Add(option);
+                    
+                }
+                return this;
+                //return Connections.FirstOrDefault(c => !c.Opened);
+                /*
+                var max = 0;
+                Valve target = null;
+                foreach(var valve in Connections)
+                {
+                    if (valve.Opened == false && valve.Rate > max)
+                    {
+                        max = valve.Rate;
+                        target = valve;
+                    }
+                }
+                return target;
+                */
+            }
+        }
+        internal class ValveOption
+        {
+            public ValveOption(Valve valve, int rate)
+            {
+                Target = valve;
+                Rate = rate;
+            }
+
+            public Valve Target { get; }
+            public int Rate { get; set; }
+            public override string ToString()
+            {
+                return $"{Target} {Rate}";
+            }
+        }
+        internal class Chain
+        {
+            public List<ValveDay> Valves { get; set; }
+        }
+        internal class ValveDay
+        {
+            public ValveDay(Valve from, Valve to, int rollRate, int days)
+            {
+                From = from;
+                At = to;
+                RollRate = rollRate;
+                Days = days;
+            }
+            public string Edge
+            {
+                get
+                {
+                    return $"{From?.Name}-{At?.Name}";
+                }
+            }
+            public int Days { get; set; }
+            public Valve From { get; set; }
+            public Valve At { get; set; }
+            public int RollRate { get; set; }
+            public override string ToString()
+            {
+                return $"at:{At} edge:{Edge} d:{Days} rollRate:{RollRate}";
+            }
+        }
+        static public void Run()
+        {
+            Day1();
+            //Day2();
+        }
+        static void Day1()
+        {
+            var input = File.ReadAllLines("DayFake16.txt");
+            var valveList = new ValveList(input);
+            //valveList.Prune();
+            var currentValve = valveList.Valves.First(v => v.Name == "AA");
+            var flow = 0;
+            for(int i = 0; i < 30; i++)
+            {
+                flow += valveList.Flow();
+                if (currentValve.Rate == 0 || currentValve.Opened)
+                {
+                    var possible = currentValve.Move(30 - i);
+                    //var possible = currentValve.Move();
+                    if (possible != currentValve)
+                    {
+                        currentValve = possible;
+                        continue;
+                    }
+                }
+                if (!currentValve.Opened)
+                {
+                    var possible = currentValve.Move(30 - i);
+                    if (possible.Rate > currentValve.Rate)
+                        currentValve = possible;
+                    else
+                        currentValve.Open();
+                }
+                Helper.Log($"Flow at {i} = {flow} on {currentValve?.Name}");
+            }
+            Helper.Log("Star1 Score: " + flow); 
+        }
+        static void Day2()
+        {
+            var input = File.ReadAllLines("DayFake15.txt");
+            var score = 0;
+            Helper.Log("Star2 Score: " + score);
+        }
+    }
+}
+
