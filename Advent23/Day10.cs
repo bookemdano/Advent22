865031df1365376f67faffd7c82f54e0e7f3d041
Daniel Francis <dfranc28@jh.edu>
2023-12-09 21:21:28 -0500

    Add Day 10

---
+++ Advent23/Day10.cs
@@ -0,0 +1,27 @@
+using AoCLibrary;
+namespace Advent23
+{
+	internal class Day10 : IDayRunner
+	{
+		public bool IsReal => false;
+		// Day https://adventofcode.com/2023/day/10
+		// Input https://adventofcode.com/2023/day/10/input
+		public object? Star1()
+		{
+			var rv = 0L;
+			var lines = Program.GetLines(StarEnum.Star1, IsReal);
+            if (!IsReal)
+                Utils.Assert(rv, 0L);
+			return rv;
+		}
+		public object? Star2()
+		{
+			var rv = 0L;
+			var lines = Program.GetLines(StarEnum.Star2, IsReal);
+            if (!IsReal)
+                Utils.Assert(rv, 0L);
+			return rv;
+		}
+
+	}
+}
