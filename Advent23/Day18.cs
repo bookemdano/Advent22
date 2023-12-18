117f694753f161ca64db8b7e37e4568b55b4a23a
Daniel Francis <dfranc28@jh.edu>
2023-12-17 21:30:31 -0500

    Day 18 added

---
+++ Advent23/Day18.cs
@@ -0,0 +1,43 @@
+using AoCLibrary;
+namespace Advent23
+{
+	internal class Day18 : IDayRunner
+	{
+		public bool IsReal => false;
+
+		// Day https://adventofcode.com/2023/day/18
+		// Input https://adventofcode.com/2023/day/18/input
+		public object? Star1()
+		{
+			var key = new StarCheckKey(StarEnum.Star1, IsReal);
+			StarCheck check;
+			if (!IsReal)
+				check = new StarCheck(key, 0L);
+			else
+				check = new StarCheck(key, 0L);
+
+			var lines = Program.GetLines(check.Key);
+			var rv = 0L;
+			// magic
+
+			check.Compare(rv);
+			return rv;
+		}
+		public object? Star2()
+		{
+			var key = new StarCheckKey(StarEnum.Star2, IsReal);
+			StarCheck check;
+			if (!IsReal)
+				check = new StarCheck(key, 0L);
+			else
+				check = new StarCheck(key, 0L);
+
+			var lines = Program.GetLines(check.Key);
+			var rv = 0L;
+			// magic
+
+			check.Compare(rv);
+			return rv;
+		}
+	}
+}
