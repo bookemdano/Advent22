0f39d7dd0439aaa4c0f9622ec9ec13bde865bf1d
Dano <bookemdano@gmail.com>
2024-12-25 05:23:16 -0500

    Done Day 24

--- Advent24/Day24.cs
+++ Advent24/Day24.cs
@@ -1,10 +1,9 @@
 using AoCLibrary;
-using Microsoft.Win32;
 namespace Advent24;
 
 internal class Day24 : IDayRunner
 {
-	public bool IsReal => false;
+	public bool IsReal => true;
 
 	// Day https://adventofcode.com/2024/day/24
 	// Input https://adventofcode.com/2024/day/24/input
@@ -22,7 +21,7 @@ internal class Day24 : IDayRunner
 		var rv = 0L;
 		// magic
 		var values = new Dictionary<string, int>();
-		var ops = new List<Ops24>();
+		var ops = new List<Op24>();
 		foreach (var line in lines)
 		{
 			if (line.Contains(':'))
@@ -31,7 +30,7 @@ internal class Day24 : IDayRunner
 				values.Add(parts[0], int.Parse(parts[1]));
 			}
 			else if (!string.IsNullOrWhiteSpace(line))
-				ops.Add(new Ops24(line));
+				ops.Add(new Op24(line));
 		}
 		while (ops.Any(o => o.Val == null && o.Res.StartsWith('z')))
 		{
@@ -41,7 +40,6 @@ internal class Day24 : IDayRunner
 				{
 					op.Connect(values[op.Lh], values[op.Rh]);
 					values[op.Res] = (int) op.Val;
-
 				}
 			}
 		}
@@ -56,9 +54,9 @@ internal class Day24 : IDayRunner
 		OR,
 		XOR
 	}
-	public class Ops24
+	public class Op24
 	{
-		public Ops24(string str)
+		public Op24(string str)
 		{
 			var parts = str.Split(" ->".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
 			Lh = parts[0];
@@ -91,7 +89,26 @@ internal class Day24 : IDayRunner
 				Val = lh ^ rh;
 		}
 	}
-	long GetBinary(char c, Dictionary<string, int> values)
+    List<ValueStream24> GetValues(char c, Dictionary<string, ValueStream24> values)
+    {
+        var od = values.Where(kvp => kvp.Key.StartsWith(c)).OrderByDescending(kvp => kvp.Key).ToDictionary();
+        var list = od.Values.ToList();
+        return list;
+    }
+    long GetBinary(char c, Dictionary<string, ValueStream24> values)
+    {
+        var od = values.Where(kvp => kvp.Key.StartsWith(c)).OrderByDescending(kvp => kvp.Key).ToDictionary();
+		var list = GetValues(c, values);
+		long rv = 0;
+        for (int i = 0; i < list.Count; i++)
+        {
+            var v = list[list.Count - i - 1];
+            if (v.Val == 1)
+                rv += (long)Math.Pow(2, i);
+        }
+        return rv;
+    }
+    long GetBinary(char c, Dictionary<string, int> values)
 	{
 		var od = values.Where(kvp => kvp.Key.StartsWith(c)).OrderByDescending(kvp => kvp.Key).ToDictionary();
 		var list = od.Values.ToList();
@@ -104,6 +121,24 @@ internal class Day24 : IDayRunner
 		}
 		return rv;
 	}
+	public class ValueStream24
+	{
+        public int Val { get; }
+        public Op24? Op { get; }
+        public ValueStream24(int val)
+        {
+            Val = val;
+        }
+        public ValueStream24(Op24 op)
+        {
+            Val = op.Val??-1;
+            Op = op;
+        }
+        public override string ToString()
+        {
+            return $"{Val} by {Op}";
+        }
+    }
 	public object? Star2()
 	{
 		var key = new StarCheckKey(StarEnum.Star2, IsReal, null);
@@ -117,23 +152,62 @@ internal class Day24 : IDayRunner
 		//var text = Program.GetText(check.Key);
 		var rv = 0L;
 		// magic
-		var values = new Dictionary<string, int>();
-		var ops = new List<Ops24>();
+		var values = new Dictionary<string, ValueStream24>();
+		var ops = new List<Op24>();
 		foreach (var line in lines)
 		{
 			if (line.Contains(':'))
 			{
 				var parts = line.Split(':', StringSplitOptions.TrimEntries);
-				values.Add(parts[0], int.Parse(parts[1]));
+				values.Add(parts[0], new ValueStream24(int.Parse(parts[1])));
 			}
 			else if (!string.IsNullOrWhiteSpace(line))
-				ops.Add(new Ops24(line));
+				ops.Add(new Op24(line));
 		}
-		var x = GetBinary('x', values);
+        while (ops.Any(o => o.Val == null && o.Res.StartsWith('z')))
+        {
+            foreach (var op in ops.Where(o => o.Val == null))
+            {
+                if (values.ContainsKey(op.Lh) && values.ContainsKey(op.Rh))
+                {
+                    op.Connect(values[op.Lh].Val, values[op.Rh].Val);
+					values[op.Res] = new ValueStream24(op);
+                }
+            }
+        }
+
+        var x = GetBinary('x', values);
 		var y = GetBinary('y', values);
 		var planZ = x & y;
+        var zs = GetValues('z', values);
+        var levels = new Dictionary<int, List<ValueStream24>>();
+        foreach (var z in zs)
+		{
+			 Console.WriteLine(z);
 
-
+			var vs = new List<ValueStream24>();
+			vs.Add(z);
+			var level = 0;
+			while (vs.Any())
+			{
+				if (levels.ContainsKey(level))
+					levels[level].AddRange(vs.ToList());
+				else
+					levels[level] = vs.ToList();
+				level++;
+                var newVs = new List<ValueStream24>();
+				foreach (var v in vs)
+				{
+					if (v.Op != null)
+					{
+                        Console.WriteLine(v.Op);
+                        newVs.Add(values[v.Op.Lh]);
+						newVs.Add(values[v.Op.Rh]);
+					}
+				}
+				vs = newVs;
+			}
+		}
 		check.Compare(rv);
 		return rv;
 	}
