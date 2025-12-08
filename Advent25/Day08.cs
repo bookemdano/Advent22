67bda734c777f2b84ef88e2d3ed8056a0f3c7eb6
Dano <bookemdano@gmail.com>
2025-12-08 11:33:17 -0500

    Day 8. Misunderstood part two but it worked anyway somehow. Refactored to make it fast and correct.

--- Advent25/Day08.cs
+++ Advent25/Day08.cs
@@ -1,4 +1,5 @@
 using AoCLibrary;
+using System.Collections.Generic;
 
 namespace Advent25;
 
@@ -20,85 +21,37 @@ internal class Day08 : IDayRunner
 		var lines = Program.GetLines(check.Key);
 		//var text = Program.GetText(check.Key);
 		var rv = 0L;
-		// magic
-        var pts = new List<Point3D>();
-        foreach(var line in lines)
-        {
-            var pt = Point3D.FromXYZ(line);
-            pts.Add(pt);
-        }
-        var dists = new List<DistanceSet>(); 
-        for (int i = 0; i < pts.Count; i++)
-        {
-            for(int j = i + 1; j < pts.Count; j++)
-            {
-                dists.Add(new DistanceSet(pts[i], pts[j]));
-            }
-        }
-        dists = dists.OrderBy(d => d.Distance).ToList();
-        var circuits = new List<List<Point3D>>();
+        // magic
         var maxConnects = 10;
         if (isReal)
             maxConnects = 1000;
-        for(int i = 0; i < maxConnects; i++)
-        {
-            var dist = dists[i];
 
-            if (!circuits.Any(c => c.Contains(dist.P1) || c.Contains(dist.P2)))
-            {
-                var circuit = new List<Point3D>();
-                circuit.Add(dists[i].P1);
-                circuit.Add(dists[i].P2);
-                circuits.Add(circuit);
-            }
-            if (circuits.Any(c => c.Contains(dist.P1) && c.Contains(dist.P2)))
-                continue;
-            var cir = circuits.FirstOrDefault(c => c.Contains(dist.P1));
-            if (cir != null)
-            {
-                if (!cir.Contains(dist.P2))
-                    cir?.Add(dist.P2);
-            }
-            cir = circuits.FirstOrDefault(c => c.Contains(dist.P2));
-            if (cir != null)
-            {
-                if (!cir.Contains(dist.P1))
-                    cir?.Add(dist.P1);
-            }
-        }
-        var changed = true;
-        while(changed)
+        var pts = lines.Select(l => Point3D.FromXYZ(l)).ToList();
+        var pairs = new List<Pair3D>();
+        var circuits = new CircuitList();
+        for (int i = 0; i < pts.Count; i++)
         {
-            changed = false;
-            for(int i = 0; i < circuits.Count; i++)
-            {
-                for(int j = i + 1; j < circuits.Count; j++)
-                {
-                    var cir1 = circuits[i];
-                    var cir2 = circuits[j];
-                    if (cir1.Intersect(cir2).Any())
-                    {
-                        cir1.AddRange(cir2.Where(p => !cir1.Contains(p)));
-                        circuits.RemoveAt(j);
-                        changed = true;
-                        break;
-                    }
-                }
-                if (changed)
-                    break;
-            }
+            circuits.Add(pts[i]);
+            for (int j = i + 1; j < pts.Count; j++)
+                pairs.Add(new Pair3D(pts[i], pts[j]));
         }
-        circuits = circuits.OrderByDescending(c => c.Count).Take(3).ToList();
-        rv = circuits[0].Count * circuits[1].Count * circuits[2].Count;
+
+        pairs = pairs.OrderBy(d => d.Distance).Take(maxConnects).ToList();
+        foreach(var pair in pairs)
+            circuits.Link(pair);
+
+        //CombineCircuits(circuits);
+        rv = circuits.TopThreeSum();
+        
         var res = new RunnerResult();
         //1440 too low
         res.StarValue = rv;
         res.StarSuccess = check.Compare(rv);
         return res;
     }
-    class DistanceSet
+    class Pair3D
     {
-        public DistanceSet(Point3D p1, Point3D p2)
+        public Pair3D(Point3D p1, Point3D p2)
         {
             P1 = p1;
             P2 = p2;
@@ -112,9 +65,93 @@ internal class Day08 : IDayRunner
         public Point3D P2;
         public double Distance;
     }
+    class CircuitList
+    {
+        List<Circuit> _circuits = [];
+
+        public int Count()
+        {
+            return _circuits.Count;
+        }
+        public CircuitList(CircuitList other)
+        {
+            foreach(var c in other._circuits)
+            {
+                _circuits.Add(new Circuit(c));
+            }
+        }
+
+        public CircuitList()
+        {
+        }
+
+        internal void Add(Point3D pt)
+        {
+            _circuits.Add(new Circuit(pt));            
+        }
+
+        internal void Link(Pair3D pair)
+        {
+            if (_circuits.Any(c => c.Contains(pair.P1) && c.Contains(pair.P2)))
+                return; // already linked
+            var cir1 = _circuits.SingleOrDefault(c => c.Contains(pair.P1));
+            var cir2 = _circuits.SingleOrDefault(c => c.Contains(pair.P2));
+            Utils.Assert(cir1 != null && cir2 != null, "circuits");
+            cir1.Combine(cir2);
+            _circuits.Remove(cir2);
+        }
+
+        internal long TopThreeSum()
+        {
+            var tops = _circuits.OrderByDescending(c => c.Points.Count).Take(3).ToList();
+            return tops[0].Points.Count * tops[1].Points.Count * tops[2].Points.Count;
+        }
+
+        internal bool AnyContain(Pair3D pair)
+        {
+            return _circuits.Any(c => c.Contains(pair.P1) && c.Contains(pair.P2));
+        }
+
+        internal void CombineCircuits()
+        {
+            var changed = true;
+            while (changed)
+            {
+                changed = false;
+                for (int i = 0; i < _circuits.Count; i++)
+                {
+                    for (int j = i + 1; j < _circuits.Count; j++)
+                    {
+                        var cir1 = _circuits[i];
+                        var cir2 = _circuits[j];
+                        if (cir1.Intersect(cir2).Any())
+                        {
+                            cir1.Combine(cir2);
+                            _circuits.RemoveAt(j);
+                            changed = true;
+                            break;
+                        }
+                    }
+                    if (changed)
+                        break;
+                }
+            }
+        }
+    }
+
     class Circuit
     {
         public List<Point3D> Points = new List<Point3D>();
+
+        public Circuit(Point3D point3D)
+        {
+            Points.Add(point3D);
+        }
+
+        public Circuit(Circuit other)
+        {
+            Points.AddRange(other.Points.ToList());
+        }
 
         internal void Add(Point3D pt)
         {
@@ -129,7 +166,7 @@ internal class Day08 : IDayRunner
             return Points.Intersect(other.Points);
         }
 
-        internal void AddIntersect(Circuit other)
+        internal void Combine(Circuit other)
         {
             Points.AddRange(other.Points.Where(p => !Points.Contains(p)));
         }
@@ -145,88 +182,30 @@ internal class Day08 : IDayRunner
         if (!isReal)
             check = new StarCheck(key, 25272L);
         else
-            check = new StarCheck(key, 0L);
+            check = new StarCheck(key, 2573952864L);
 
         var lines = Program.GetLines(check.Key);
         //var text = Program.GetText(check.Key);
         var rv = 0L;
         // magic
-        var pts = new List<Point3D>();
-        foreach (var line in lines)
-        {
-            var pt = Point3D.FromXYZ(line);
-            pts.Add(pt);
-        }
-        var dists = new List<DistanceSet>();
-        var circuits = new List<Circuit>();
+        var pts = lines.Select(l => Point3D.FromXYZ(l)).ToList();
+        var pairs = new List<Pair3D>();
+        var circuits = new CircuitList();
         for (int i = 0; i < pts.Count; i++)
         {
-            var circuit = new Circuit();
-            circuit.Add(pts[i]);
-            circuits.Add(circuit);
+            circuits.Add(pts[i]);
             for (int j = i + 1; j < pts.Count; j++)
-            {
-                dists.Add(new DistanceSet(pts[i], pts[j]));
-            }
+                pairs.Add(new Pair3D(pts[i], pts[j]));
         }
-        dists = dists.OrderBy(d => d.Distance).ToList();
-        var maxConnects = 10;
-        if (isReal)
-            maxConnects = 1000;
-        for (int i = 0; i < maxConnects; i++)
-        {
-            var dist = dists[i];
 
-            if (!circuits.Any(c => c.Contains(dist.P1) || c.Contains(dist.P2)))
-            {
-                var circuit = new Circuit();
-                circuit.Add(dist.P1);
-                circuit.Add(dist.P2);
-                circuits.Add(circuit);
-            }
-            if (circuits.Any(c => c.Contains(dist.P1) && c.Contains(dist.P2)))
-                continue;
-            var cir = circuits.FirstOrDefault(c => c.Contains(dist.P1));
-            if (cir != null)
-            {
-                if (!cir.Contains(dist.P2))
-                    cir?.Add(dist.P2);
-            }
-            cir = circuits.FirstOrDefault(c => c.Contains(dist.P2));
-            if (cir != null)
-            {
-                if (!cir.Contains(dist.P1))
-                    cir?.Add(dist.P1);
-            }
-        }
-        CombineCircuits(circuits);
-        long iDist = 0;
-        foreach (var dist in dists)
+        pairs = pairs.OrderBy(d => d.Distance).ToList();
+        foreach(var pair in pairs)
         {
-            iDist++;
-            var cir = circuits.FirstOrDefault(c => c.Contains(dist.P1) && c.Contains(dist.P2));
-            if (cir == null)
+            circuits.Link(pair);
+            if (circuits.Count() == 1)
             {
-                //if (!circuits.All(c => c.Contains(dist.P1) || c.Contains(dist.P2)))
-                //    continue;
-                var copyCircuits = new List<Circuit>();
-                foreach(var iCir in circuits)
-                {
-                    var newCir = new Circuit();
-                    newCir.Points.AddRange(iCir.Points.ToList());
-                    copyCircuits.Add(newCir);
-                }
-
-                var circuit = new Circuit();
-                circuit.Add(dist.P1);
-                circuit.Add(dist.P2);
-                copyCircuits.Add(circuit);
-                CombineCircuits(copyCircuits);
-                if (copyCircuits.Count == 1)
-                {
-                    rv = dist.P1.X * dist.P2.X;
-                    break;
-                }
+                rv = pair.P1.X * pair.P2.X;
+                break;
             }
         }
 
@@ -234,32 +213,6 @@ internal class Day08 : IDayRunner
         res.StarValue = rv;
         res.StarSuccess = check.Compare(rv);
         return res;
-    }
-    
-    private static void CombineCircuits(List<Circuit> circuits)
-    {
-        var changed = true;
-        while (changed)
-        {
-            changed = false;
-            for (int i = 0; i < circuits.Count; i++)
-            {
-                for (int j = i + 1; j < circuits.Count; j++)
-                {
-                    var cir1 = circuits[i];
-                    var cir2 = circuits[j];
-                    if (cir1.Intersect(cir2).Any())
-                    {
-                        cir1.AddIntersect(cir2);
-                        circuits.RemoveAt(j);
-                        changed = true;
-                        break;
-                    }
-                }
-                if (changed)
-                    break;
-            }
-        }
     }
 }
 
