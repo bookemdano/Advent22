fbd7b1dbd983e18181574c1877a59039bbbf1813
Dano <bookemdano@gmail.com>
2025-12-24 19:52:22 -0500

    Y20D10 simple day 2

--- Advent20/Day10.cs
+++ Advent20/Day10.cs
@@ -1,4 +1,5 @@
 using AoCLibrary;
+using Microsoft.Win32;
 namespace Advent20;
 // #working
 internal class Day10 : IRunner
@@ -10,24 +11,57 @@ internal class Day10 : IRunner
         var key = new StarCheckKey(StarEnum.Star1, isReal, null);
         var res = new RunnerResult();
         if (!isReal)
-			res.Check = new StarCheck(key, -1L);
+			res.Check = new StarCheck(key, 220L);
 		else
-			res.Check = new StarCheck(key, -1L);
+			res.Check = new StarCheck(key, 1820L);
 
 		var lines = RunHelper.GetLines(key);
 		//var text = RunHelper.GetText(key);
 		var rv = 0L;
 		// magic
-
+		var lngs = lines.Select(l => long.Parse(l)).OrderBy(l => l).ToList();
+		var last = 0L;
+		var diff1s = 0L;
+		var diff3s = 0L;
+		for(int i = 0; i < lngs.Count; i++)
+		{
+			var delta = lngs[i] - last;
+			if (delta == 1)
+				diff1s++;
+			else if (delta == 3)
+				diff3s++;
+			else
+				diff1s++;
+			last = lngs[i];
+        }
+        diff3s++;
+		rv = diff1s * diff3s;
         res.CheckGuess(rv);
         return res;
     }
+	long Connect(List<long> lngs, long last)
+	{
+		long rv = 0L;
+        if (lngs.Count() == 1)
+            return 1;
+        if (lngs.Count() == 0)
+            return 0;
+		var max = int.Min(3, lngs.Count());
+        for (int i = 0; i < max; i++)
+		{
+			if (lngs[i] > last + 3)
+				continue;
+			rv += Connect(lngs.Skip(i+1).ToList(), lngs[i]);
+        }
+		return rv;
+	}
+
     public RunnerResult Star2(bool isReal)
     {
         var key = new StarCheckKey(StarEnum.Star2, isReal, null);
         var res = new RunnerResult();
         if (!isReal)
-			res.Check = new StarCheck(key, -1L);
+			res.Check = new StarCheck(key, 8L);
 		else
 			res.Check = new StarCheck(key, -1L);
 
@@ -35,8 +69,9 @@ internal class Day10 : IRunner
 		//var text = RunHelper.GetText(key);
 
 		var rv = 0L;
-		// magic
-
+        // magic
+        var lngs = lines.Select(l => long.Parse(l)).OrderBy(l => l).ToList();
+		rv = Connect(lngs, 0);
         res.CheckGuess(rv);
         return res;
 	}
