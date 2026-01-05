bc4fd984cdcf37f9e23e998572df9578d85861d9
Dano <bookemdano@gmail.com>
2026-01-05 07:18:55 -0500

    Y20D19 Need to read instructions better and go back

--- Advent20/Day19.cs
+++ Advent20/Day19.cs
@@ -1,5 +1,6 @@
 using AoCLibrary;
 using System.Data;
+
 namespace Advent20;
 // #working
 internal class Day19 : IRunner
@@ -72,33 +73,44 @@ internal class Day19 : IRunner
     }
     static class RuleSet19
     {
-        static Dictionary<int, Rule19> _dict = [];
+        static List<Rule19> _rules = [];
+
+        public static int MaxLen { get; internal set; } = 1000;
+
         static public void Add(string line)
         {
             var parts = line.Split(": ");
             var key = int.Parse(parts[0]);
-            var val = new Rule19(parts[1]);
-            _dict.Add(key, val);
+            Remove(key);
+            var val = new Rule19(key, parts[1]);
+            _rules.Add(val);
         }
 
         internal static void Clear()
         {
-            _dict.Clear();
+            _rules.Clear();
         }
         static public Rule19 GetRule(int ruleId)
         {
-            return _dict[ruleId];
+            return _rules.Single(r => r.Id == ruleId);
+        }
+
+        internal static void Remove(int ruleId)
+        {
+            _rules.RemoveAll(r => r.Id == ruleId);
         }
     }
 
     class Rule19
 	{
+        public int Id { get; }
         public char Target { get; } = '-';
         public List<SubRuleIdSet> SubRuleIds { get; } = [];
 
 
-        public Rule19(string rule)
+        public Rule19(int key, string rule)
         {
+            Id = key;
             if (rule.Contains('\"'))
             {
                 var subParts = rule.Split('\"', StringSplitOptions.RemoveEmptyEntries).ToList();
@@ -122,23 +134,55 @@ internal class Day19 : IRunner
         }
 
         internal bool HasTarget => Target != '-';
-        //static int _maxLen;
         internal bool Matches(string msg)
         {
-            var targetStrings = GetRuleStrings();
-            //var maxLen = targetStrings.Max(s => s.Length);
-            //if (maxLen > _maxLen)
-            //   _maxLen = maxLen;
+            var targetStrings = GetRuleStrings(0);
             //Utils.Assert(targetStrings.Distinct().Count() == targetStrings.Count(), "Just enough");
 
             return targetStrings.Contains(msg);
         }
+        internal bool MatchAlong(string target)
+        {
+            var rv = SubMatchAlong(target, []);
+            return rv.Any();
+        }
+        HashSet<string> SubMatchAlong(string target, List<int> checkeds)
+        {
+            if (checkeds.Contains(Id))
+                return new HashSet<string> { "X" };
+            checkeds.Add(Id);
+            if (HasTarget)
+            {
+                return new HashSet<string> { Target.ToString() };
+            }
+            else
+            {
+                var rv = new List<string>();
+                foreach (var orRule in SubRuleIds)
+                {
+                    var strs = new HashSet<string>() { string.Empty };
+                    foreach (var andRule in orRule.GetRules())
+                    {
+                        var ruleStrings = andRule.SubMatchAlong(target, checkeds.ToList());
+                        strs = AddToAll(strs, ruleStrings);
+                    }
+                    var matches = strs.Where(s => target.StartsWith(s)).ToList();
+                    rv.AddRange(strs.Where(s => target.StartsWith(s)));
+                }
+                return rv.Distinct().ToHashSet();
+            }
+        }
+
         HashSet<string>? _ruleStrings;
-        internal HashSet<string> GetRuleStrings()
+        internal HashSet<string> GetRuleStrings(int depth)
         {
             if (_ruleStrings == null)
             {
-                if (HasTarget)
+                if (depth++ > RuleSet19.MaxLen)
+                {
+                    _ruleStrings = new HashSet<string>();
+                }
+                else if (HasTarget)
                 {
                     _ruleStrings = new HashSet<string> { Target.ToString() };
                 }
@@ -150,24 +194,32 @@ internal class Day19 : IRunner
                         var strs = new HashSet<string>() { string.Empty };
                         foreach (var andRule in orRule.GetRules())
                         {
-                            var ruleStrings = andRule.GetRuleStrings();
+                            var ruleStrings = andRule.GetRuleStrings(depth);
                             strs = AddToAll(strs, ruleStrings);
                         }
                         rv.AddRange(strs);
                     }
-                    Utils.Assert(rv.Distinct().Count() == rv.Count(), "Just enough");
-
-                    _ruleStrings = rv.ToHashSet();
+                    _ruleStrings = rv.Distinct().ToHashSet();
                 }
             }
             return _ruleStrings;
         }
         HashSet<string> AddToAll(HashSet<string> strs, string added)
         {
-            return strs.Select(s => s + added).ToHashSet();
+            var rv = new HashSet<string>();
+            foreach(var str in strs)
+            {
+                var newStr = str + added;
+                if (newStr.Length > RuleSet19.MaxLen)
+                    continue;
+                rv.Add(newStr);
+            }
+            return rv;
         }
         HashSet<string> AddToAll(HashSet<string> strs, HashSet<string> addeds)
         {
+            if (!addeds.Any())
+                return strs;
             var rv = new List<string>();
             foreach (var added in addeds)
                 rv.AddRange(AddToAll(strs, added));
@@ -199,14 +251,17 @@ internal class Day19 : IRunner
             else
                 msgs.Add(line);
         }
-        var rule8 = RuleSet19.GetRule(8);
-        //rules.Remove(rule8);
-
-
+        // overwrite these rules
+        RuleSet19.Add("8: 42 | 42 8");
+        RuleSet19.Add("11: 42 31 | 42 11 31");
+        RuleSet19.MaxLen = msgs.Max(m => m.Length);
         var rule0 = RuleSet19.GetRule(0);
         foreach (var msg in msgs)
-            if (rule0.Matches(msg))
+            if (rule0.MatchAlong(msg))
+            {
+                ElfHelper.DayLogPlus(msg);
                 rv++;
+            }
         res.CheckGuess(rv);
         return res;
 	}
