b1b50c8d136a6e158fe7ef774fba53eb32881f02
Dano <bookemdano@gmail.com>
2026-01-05 07:34:18 -0500

    Y19D01

---
+++ Advent19/Day01.cs
@@ -0,0 +1,62 @@
+using AoCLibrary;
+namespace Advent19;
+// #working
+internal class Day01 : IRunner
+{
+	// Day https://adventofcode.com/2019/day/1
+	// Input https://adventofcode.com/2019/day/1/input
+	public RunnerResult Star1(bool isReal)
+	{
+		var key = new StarCheckKey(StarEnum.Star1, isReal, null);
+		var res = new RunnerResult();
+		if (!isReal)
+			res.Check = new StarCheck(key, 34241L);
+		else
+			res.Check = new StarCheck(key, 3271994L);
+
+		var lines = RunHelper.GetLines(key);
+		//var text = RunHelper.GetText(key);
+		var rv = 0L;
+		// magic
+		var lngs = lines.Select(l => long.Parse(l)).ToList();
+		foreach (var lng in lngs)
+		{
+			var fuel = (lng / 3) - 2;
+			rv += fuel;
+        }
+
+        res.CheckGuess(rv);
+        return res;
+    }
+    public RunnerResult Star2(bool isReal)
+    {
+        var key = new StarCheckKey(StarEnum.Star2, isReal, null);
+        var res = new RunnerResult();
+        if (!isReal)
+			res.Check = new StarCheck(key, 51316L);
+		else
+			res.Check = new StarCheck(key, 4905116L);
+
+		var lines = RunHelper.GetLines(key);
+		//var text = RunHelper.GetText(key);
+
+		var rv = 0L;
+        // magic
+        var lngs = lines.Select(l => long.Parse(l)).ToList();
+        foreach (var lng in lngs)
+        {
+            var added = (lng / 3) - 2;
+            var fuel = 0L;
+            while (added > 0)
+			{
+                fuel += added;
+                added = (added / 3) - 2;
+            }
+            rv += fuel;
+        }
+
+        res.CheckGuess(rv);
+        return res;
+	}
+}
+
---
+++ Advent19/Program.cs
@@ -0,0 +1,20 @@
+﻿using AoCLibrary;
+
+namespace Advent19;
+
+internal class Program
+{
+    static async Task Main()
+    {
+        Utils.AppName = "RUN";
+        ElfHelper.OverrideYear(2019);
+        ElfHelper.UpdateCurrentDay();
+        var runner = RunHelper.GetDayRunner(ElfHelper.DayString);
+        if (runner == null)
+            ElfHelper.DayLog("No runner found for Day" + ElfHelper.DayString);
+        else
+        {
+            await RunHelper.RunAsync(runner);
+        }
+    }
+}
